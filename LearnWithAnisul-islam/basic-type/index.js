93f7073f6795c0c8a2815c72e2bc9e4248065567
Raihan567 <aburaihansarker567@gmail.com>
2022-06-14 17:25:22 +0600

    typeScript basic type

---
+++ LearnWithAnisul-islam/basic-type/index.js
@@ -0,0 +1,16 @@
+// Built in data type: Number, String, Boolean, Null, Undefined, void
+// Number
+var userId;
+userId = 1234324;
+// String:
+var userName;
+userName = "Raihan";
+// Boolean
+var userVerification;
+userVerification = true;
+// Null
+var userNull;
+userNull = null;
+// Undefined:
+var userUndefined;
+userUndefined = undefined;
---
+++ LearnWithAnisul-islam/basic-type/index.ts
@@ -0,0 +1,22 @@
+// Built in data type: Number, String, Boolean, Null, Undefined, void
+
+// Number
+let userId: number;
+userId = 1234324;
+
+// String:
+let userName: string;
+userName = "Raihan"
+
+// Boolean
+let userVerification : boolean;
+userVerification = true; 
+
+// Null
+let userNull : null;
+userNull = null
+
+// Undefined:
+let userUndefined: undefined;
+userUndefined = undefined;
+
