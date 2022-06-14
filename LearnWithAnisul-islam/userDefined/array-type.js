99bd26797b28eb559ade4d4254d6a8acd36cd85d
Raihan567 <aburaihansarker567@gmail.com>
2022-06-14 17:52:27 +0600

    array data type

---
+++ LearnWithAnisul-islam/userDefined/array-type.js
@@ -0,0 +1,7 @@
+var userNames;
+var user1;
+var user2;
+userNames = ["raihan", "Sumaiya", "Abdullah", "Sara"];
+user1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+user2 = [true, false, true, false];
+console.log("".concat(userNames, ", ").concat(user1, ", ").concat(user2));
---
+++ LearnWithAnisul-islam/userDefined/array-type.ts
@@ -0,0 +1,9 @@
+let userNames: string[];
+let user1: number[];
+let user2: boolean[];
+
+userNames = ["raihan", "Sumaiya", "Abdullah", "Sara"];
+user1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+user2 = [true, false, true, false];
+
+console.log(`${userNames}, ${user1}, ${user2}`);
